47195f5afd0a38f5092082bfb909f6347584c39d
maximeOC <maxime1513@gmail.com>
2023-01-20 18:45:34 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { ChatEngine } from "react-chat-engine";
+import './index.css'
+
+
+const App = () => {
+    return(
+        <ChatEngine 
+            height="100vh"
+            projectID="61269157-4966-4977-a51f-9772cbc8d88c"
+            userName="maxime"
+            userSecret="12345"
+
+        />
+    )
+}
+export default App;
